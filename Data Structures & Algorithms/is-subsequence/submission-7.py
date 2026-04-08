d23e089428c9f8b6350699833d07edb466ed1201
Christy Dominic Cyril <143304735+callmekriztal@users.noreply.github.com>
2026-04-08 15:57:34 +0000

    Add: is-subsequence - submission-7

---
+++ Data Structures & Algorithms/is-subsequence/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = 0
+        tp = 0 
+        isSub = False
+        while tp < len(t) and sp < len(s):
+            if s[sp] == t[tp]:
+                sp += 1
+                tp += 1
+            else:
+                tp += 1
+        
+        if sp != len(s):
+            return False
+        else :
+            return True
+            
